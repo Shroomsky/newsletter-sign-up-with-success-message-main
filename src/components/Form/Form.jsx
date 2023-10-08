f3f0475c8e5629ad871e2db800776cfa477e3877
Piotr Grzybowski <grzybowski_p@wp.pl>
2023-10-08 20:10:08 +0200

    Form import fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "../button/button.jsx";
+import { Button } from "../Button/Button.jsx";
 import styles from "./Form.module.css";
 import { emailCheck } from "../../utilities/emailCheck.js";
 
@@ -18,22 +18,22 @@ export function Form({ setCompleted, setEmail }) {
 		}
 	}
 
-	return (<>
-	
-		<form className={styles.form} onSubmit={submitHandler}>
-			{error && <p className={styles.error}>Valid email required</p>}
-			<label htmlFor="email">Email addres</label>
-			<input
-				className={`${styles.input} ${error && styles.input_error}`}
-				id="email"
-				type="text"
-				autoComplete="off"
-				placeholder="email@company.com"
-				value={text}
-				onChange={(e) => setText(e.target.value)}
-			/>
-			<Button>Subscribe to monthly newsletter</Button>
-		</form>
+	return (
+		<>
+			<form className={styles.form} onSubmit={submitHandler}>
+				{error && <p className={styles.error}>Valid email required</p>}
+				<label htmlFor="email">Email addres</label>
+				<input
+					className={`${styles.input} ${error && styles.input_error}`}
+					id="email"
+					type="text"
+					autoComplete="off"
+					placeholder="email@company.com"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				/>
+				<Button>Subscribe to monthly newsletter</Button>
+			</form>
 		</>
 	);
 }
